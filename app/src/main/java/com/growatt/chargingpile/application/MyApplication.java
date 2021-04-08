33bb90fc96293bbd84861f5e3087afdf992c30e4
JashonL <721695214@qq.com>
2021-04-08 13:46:02 +0800

    版本2.5更新

--- app/src/main/java/com/growatt/chargingpile/application/MyApplication.java
+++ app/src/main/java/com/growatt/chargingpile/application/MyApplication.java
@@ -46,7 +46,7 @@ public class MyApplication extends Application {
         // 初始化
         x.Ext.init(this);
         // 设置是否输出debug
-        x.Ext.setDebug(true);
+        x.Ext.setDebug(false);
         initCirclerDialog();
         //极光推送
         JPushInterface.setDebugMode(false);
